ae9bf2bc7bb5a891ad29348b2d97a4f9a1f391f3
yassin_004 <yassinmahmoud004@gmail.com>
2023-07-11 04:27:54 +0300

    commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import background from "../assets/blue-shopping-bag.jpg";
 
 const Hero = () => {
     return (
         <>
-            <section className=" h-[720px] bg-hero bg-no-repeat bg-cover bg-center py-24">
-                <div className="container mx-auto flex justify-between h-full">
+            <section className="h-[720px] bg-no-repeat bg-cover bg-center py-24">
+                <img
+                    className="absolute h-full w-full"
+                    src={background}
+                    alt="img"
+                />
+                <div className="absolute container mx-auto flex justify-between h-full">
                     <div className="flex flex-col justify-center mx-4">
                         <div className="font-semibold flex items-center uppercase">
                             <div className="w-10 h-[2px] mr-3 bg-blue-gray-700"></div>
@@ -15,7 +21,7 @@ const Hero = () => {
                             <span className="font-semibold">BRAND</span>
                         </h1>
                         <Link
-                            to={"/"}
+                            to={"/e-Commerce-React/"}
                             className="self-start uppercase font-semibold border-b-2 border-primary"
                         >
                             Discover More
